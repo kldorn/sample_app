65776ec7517544ccfce5dfceb98f28a0fd758be0
Kathleen Dorn <kdorn@usatoday.com>
2014-03-12 09:19:42 -0400

    Commit Chapter 3 execizes 3.5 1 and 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  #Added from Listing 3.31
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -21,7 +24,8 @@ describe "Static pages" do
     #Added from Rails tutorial Listing 3.19
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      #expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -36,7 +40,8 @@ describe "Help page" do
     #Added from Rails tutorial Listing 3.19
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      #expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end
@@ -51,7 +56,25 @@ describe "Help page" do
     #Added from Rails tutorial Listing 3.19
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      #expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+    
+   end
+
+    #Added from Chap 3 exercizes
+    describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    #Added from Rails tutorial Listing 3.19
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      #expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
   end
